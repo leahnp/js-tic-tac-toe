6dac2c9706c7e901dd5ac4b098735ddf3e7fb638
Leah <leahnpetersen@gmail.com>
2016-06-30 16:50:34 -0700

    trying to reset game vars and clear board when player wins

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,5 @@
 	function TicTacToe() {
-		this.player = 1;
+		this.player = 2;
 		this.game_state_obj = {
 			1: [],
 			2: []
@@ -15,43 +15,42 @@
 		} else {
 			this.player = 1;
 		};
-		console.log("player " + this.player)
+		// console.log("player " + this.player)
 
 		// add play to players tally in gamestate object
-		var plays = this.game_state_obj[this.player]
-		plays.push(parseInt(id));
-		console.log(plays)
+		// var plays = this.game_state_obj[this.player]
+		this.game_state_obj[this.player].push(parseInt(id));
+		console.log(this.game_state_obj[this.player]);
 
 		var wins = {
 			0: [1, 2, 3],
 			1: [4, 5, 6], 
 			2: [7, 8, 9], 
-
 			3: [1, 4, 7], 
 			4: [2, 5, 8], 
 			5: [3, 6, 9], 
-
 			6: [1, 5, 9], 
 			7: [3, 5, 7]
-		}
+		};
 
 		// iterate through wins object to find winner
 		for (var win in wins) {
+			// console.log(win)
 			// see if value array elements are included in players plays
-			if (plays.includes(wins[win][0, 1, 2])) {
-				console.log(this.player + " is the winner!");
-				// this.win = true;
+			if (this.game_state_obj[this.player].includes(wins[win][0, 1, 2])) {
 				this.new_game();
-				return "Player" + this.player + "wins!"
-			}
+				return 'won'
+				// need to pass the real winner to statement - this is just player2 everytime
+				return "Player" + this.player + "wins!" + JSON.stringify(this.game_state_obj) + JSON.stringify(this.played_tiles)
+			};
+		};
 
-			return  "Player 1: " + this.game_state_obj[1] + 
-							"\nPlayer 2: " + this.game_state_obj[2]
-		}
+		return  "Player 1 (black): " + this.game_state_obj[1] + 
+						"<br>Player 2 (red): " + this.game_state_obj[2];
 	};
 
 	TicTacToe.prototype.new_game = function() {
-		this.player = 1;
+		this.player = 2;
 		this.game_state_obj = {
 			1: [],
 			2: []
@@ -62,17 +61,20 @@
 
 $(document).ready(function() {
   var body = $('body');
-  var display = body.children('.display')
+  var display = body.children('.display');
 	var buttons = body.children('button');
-	var reset_button = body.children('#reset')
-	console.log('chips' + reset_button)
-	var ttt;
-	// var played_tiles = {};
+	var reset_button = body.children('#reset');
+	var ttt = new TicTacToe();
+
+
 
 	reset_button.on('click', function(event) {
 		event.preventDefault();
+		// reset game vars
 		ttt.new_game();
+		// udpate game text
 		display.text("new game");
+		// reset button faces
 		buttons.css('background', 'buttonface');
 		return 
 	});
@@ -82,20 +84,14 @@ $(document).ready(function() {
 		var class_name = this.className;
 		var id = this.id;
 
-		// start new game if user just landed on page
-		if (ttt == undefined) {
-			ttt = new TicTacToe();
-		}
-		
 		// check if tile has already been played
 		if (ttt.played_tiles[id] === 'played') {
 			display.text("already played, try different tile")
 			return
-		}
+		};
 
 		// add played tile to played object
 		ttt.played_tiles[id] = 'played';
-
 
 		// put correct play symbol on board
 		if (ttt.player == 1) {
@@ -103,9 +99,22 @@ $(document).ready(function() {
 		} else {
 			$(this).css('background', 'black');
 		}
-		console.log(id);
+		// console.log(id);
 
 		// update game state
-		display.text(ttt.game_play(id));
+		// I know the html is bad. 
+		var returnInfo = ttt.game_play(id);
+		if (returnInfo == 'won') {
+			// reset vars then reset buttons
+			// returnInfo = ttt.player + " player has won!"
+			// ttt.new_game();
+			// reset_button.on();
+		}
+		console.log(returnInfo);
+		return display.html(returnInfo);
 	})
 })
+		// start new game if user just landed on page
+		// if (ttt == undefined) {
+		// 	ttt = new TicTacToe();
+		// }
