b92330522725be3cf95e45017750b583b8dfb34e
Leah <leahnpetersen@gmail.com>
2016-06-30 13:18:44 -0700

    setup jquery vars, html elements and css to align the board, started tictactoe class with constructor

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,65 @@
-function TicTacToe() {
+// turn concept
+// whose turn is it? keep track of rounds
+// select a space/is it availablle
+// check win (define win)/loss(define loss)/draw state(define tie)
+// draw outcome
 
-}
+// player 1 = x
+// player 0 = o
 
-TicTacToe.prototype = {
-  
-}
+// obj = { 0: [1,2,3] 1: [4,5,6]}
+
+	function TicTacToe() {
+		this.player = 1;
+		this.game_state_obj = {};
+		// this._callback = callback;
+	};
+
+	TicTacToe.prototype.game_play = function(id) {
+		if (this.player == 1) {
+			this.player = 0;
+		} else {
+			this.player = 1;
+		};
+		console.log("player " + this.player)
+	};
+
+	TicTacToe.prototype.new_game = function() {
+		this.player = 1;
+		this.game_state_obj = {};
+	}
+
+$(document).ready(function() {
+  var body = $('body');
+	var buttons = body.children('button');
+	var ttt;
+
+	buttons.on('click',  function(event) {
+		event.preventDefault();
+		var class_name = this.className;
+		var id = this.id;
+		// start new game if user just landed on page
+		if (ttt == undefined) {
+			ttt = new TicTacToe();
+			// console.log('only once')
+		}
+
+		// if reset button, start new game
+		if (id == 'reset') {
+			ttt.new_game();
+		}
+
+		console.log(id);
+		// console.log('player' + ttt.player);
+
+		// update game state
+		ttt.game_play(id)
+	})
+
+	// function TicTacToe() {
+
+	// }
+
+
+
+})
